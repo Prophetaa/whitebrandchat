c66d78454f7040893c5746d5cedbe56d0d76532e
claudio <claudio@afrogleap.com>
2019-07-19 12:01:32 +0200

    fixing merging issues

--- src/helpers/AppNavigation.js
+++ src/helpers/AppNavigation.js
@@ -1,16 +1,8 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-<<<<<<< HEAD:src/helpers/AppNavigation.js
 import { HomeScreen } from '../modules/Home';
 import { LoginScreen } from '../modules/Login';
 import { SplashScreen } from '../modules/Splash';
 import { ContactsScreen } from '../modules/Contacts';
-=======
-import HomeScreen from '../screens/HomeScreen';
-import LoginScreen from '../screens/LoginScreen';
-import SplashScreen from '../screens/SplashScreen';
-import ContactsScreenStyles from '../styling/ContactsScreen.styles';
-import ContactsScreen from '../screens/ContactsScreen';
->>>>>>> master:helpers/AppNavigation.js
 
 const AppNavigator = createStackNavigator(
 	{
